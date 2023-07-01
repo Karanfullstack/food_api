547dfad812ef1af755d45ada56c2dbf0f81aa50b
Karan <karan@karans-air.home>
2023-07-01 20:44:07 +0200

    Validation Schema

--- controllers/auth/registerController.js
+++ controllers/auth/registerController.js
@@ -1,5 +1,18 @@
+import Joi from "joi";
+
+// Validation Schema
 const registerController = {
   register(req, res, next) {
+    const registerSchema = Joi.object({
+      name: Joi.string().min(3).max(30).required(),
+      email: Joi.string().email().required(),
+      password: Joi.string()
+        .pattern(new RegExp("^[a-zA-Z0-9]{3,30}"))
+        .required(),
+      repeat_password: Joi.ref("password"),
+    });
+    // End of Validation Schema
+
     res.send({message: "Hello from Express"});
   },
 };
